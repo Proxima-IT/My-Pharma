b03838c5e4bcd328833fb792528b25a61b31773e
NoosratMeem <meem.noosrat@gmail.com>
2026-02-07 19:41:37 +0600

    tailwind css issue solving

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./(public)/components/Header";
+import Footer from "./(public)/components/Footer";
+
 
 const inter = Inter({
   subsets: ["latin"],
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="bg-color-success-900 text-primary-600 font-inter p-10">
+      <div className="bg-color-success-900 text-primary-900 font-inter p-10 ">
         This should now be your dark blue (#0f193b)
       </div>
     </div>
